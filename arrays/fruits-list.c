277c3baa855e6bbe328eaed2546b621b63b45b38
orlando <ae.olando@gmail.com>
2026-05-24 00:52:56 +0300

    fix inability to add new fruits

--- arrays/fruits-list.c
+++ arrays/fruits-list.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+
+#define MAX_FRUITS 100
+
+static void add_fruit(const char *fruits[MAX_FRUITS], int *count, const char *fruit)
+{
+    if (*count >= MAX_FRUITS)
+    {
+        printf("Cannot add more fruits: capacity reached (%d)\n", MAX_FRUITS);
+        return;
+    }
+    fruits[*count] = fruit;
+    (*count)++;
+}
+
 int main(void)
 {
-    int i;
-    char *fruits[] = {"Orange",
-                      "Banana",
-                      "Pineapple",
-                      "Tomatoes"};
+    int size = 0;
+    const char *fruits[MAX_FRUITS] = {0};
 
-    fruits[3] = "Mango";
-    for (i = 0; i <= 3; i++)
+    add_fruit(fruits, &size, "Orange");
+    add_fruit(fruits, &size, "Banana");
+    add_fruit(fruits, &size, "Pineapple");
+    add_fruit(fruits, &size, "Tomatoes");
+    add_fruit(fruits, &size, "Apple");
+    add_fruit(fruits, &size, "Mapera");
+
+    for (int i = 0; i < size; i++)
     {
         printf("%s\n", fruits[i]);
     }
+
+    return 0;
 }
